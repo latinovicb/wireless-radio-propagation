43ec5cb71c7bd45ba68426b810a31b39100a85b8
BL <borek.latinovic@powerdynax.com>
2023-12-05 01:54:45 +0200

    Change form
    NOTE: still not functional

--- project_solution.m
+++ project_solution.m
@@ -1,44 +1,62 @@
 % Constants
-frequencies = [900e6, 1500e6, 3600e6]; % UMTS, LTE, 5G frequencies
+frequencies = [900e6]; % UMTS, LTE, 5G frequencies
 ht = 40; % Transmitter antenna height in meters
 hr = 1,8;  % Receiver antenna height in meters
 epsilon_r = 17.2; % Relative permittivity of the ground
+c = 3e8
 
 % Distance range
-distances = logspace(100, 1, 100); % 100 points from 100m to 10km
 
-k = (2*pi)/(c/f)
-beta_angle = atan((ht + hr) / d);
-
-R1 = (ht/sin(beta_angle))
-R2 = (hr/sin(beta_angle))
-Rd = sqrt((ht - hr)^2 + d^2)
-Ri = R1 + R2
-
-detla = Ri - Rd
 
 % Plotting for each frequency
 figure;
 for f = frequencies
-    lambda = 3e8 / f; % Wavelength
+    lambda = c / f; % Wavelength
     received_power = zeros(size(distances));
+    
+    Ev_vals = {};
+    Eh_vals = {};
 
-    for i = 1:length(distances)
-        d = distances(i);
-        theta = atan((ht - hr) / d);
-        Rh = ((cos(theta) - sqrt(epsilon_r - sin(theta)^2)) / (cos(theta) + sqrt(epsilon_r - sin(theta)^2)))^2;
-        received_power(i) = 1 / (d^2) * (1 + Rh * exp(-j * 2 * pi * d / lambda));
+    distances = {}; % points from 100m to 10km
+
+    k = (2*pi)/(c/f);
+    for d = 100:10:1000
+        distances  = [distances, d];
+
+        beta_angle = atan((ht + hr) / d);
+        R1 = (ht/sin(beta_angle));
+        R2 = (hr/sin(beta_angle));
+        Rd = sqrt((ht - hr)^2 + d^2);
+        Ri = R1 + R2;
+        delta = Ri - Rd;
+
+        Rv = (-(epsilon_r*sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2)) / ((epsilon_r*sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2));
+        Rh = (sin(beta_angle)) - sqrt(epsilon_r - cos(beta_angle).^2) / ((sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2));
+
+        E0 = (exp(-j*k*Rd)) / Rd;
+        Ev = E0 * (1 + Rv*exp(-j*k*delta));
+        Eh = E0 * (1 + Rh*exp(-j*k*delta));
+
+        Ev_vals = [Ev_vals, Ev];
+        Eh_vals = [Eh_vals, Eh];
+        
+        % theta = atan((ht - hr) / d);
+        % Rh = ((cos(theta) - sqrt(epsilon_r - sin(theta)^2)) / (cos(theta) + sqrt(epsilon_r - sin(theta)^2)))^2;
+        % received_power(i) = 1 / (d^2) * (1 + Rh * exp(-j * 2 * pi * d / lambda));
     end
 
     % Plotting
-    loglog(distances, abs(received_power), 'DisplayName', sprintf('%d MHz', f / 1e6));
-    hold on;
+    % loglog(distances, abs(received_power), 'DisplayName', sprintf('%d MHz', f / 1e6));
+    % hold on;
+    plot(Ev_vals,distances);
+    plot(Eh_vals, distances);
+
 end
 
-title('Two-Ray Reflection Model');
-xlabel('Distance (m)');
-ylabel('Received Power');
-legend('show');
-grid on;
-hold off;
+% title('Two-Ray Reflection Model');
+% xlabel('Distance (m)');
+% ylabel('Received Power');
+% legend('show');
+% grid on;
+% hold off;
 
