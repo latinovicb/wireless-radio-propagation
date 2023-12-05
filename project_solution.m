76252202fe61e8babd677e4a9bb2a024f8dc5c53
BL <borek.latinovic@powerdynax.com>
2023-12-05 18:21:45 +0200

    Plotting for path loss models & propagation models
    TODO:
    Check the values, unit transformations etc.

--- project_solution.m
+++ project_solution.m
@@ -1,11 +1,13 @@
 % Constants
 frequencies = [900e6, 1800e6, 3600e6]; % UMTS, LTE, 5G frequencies
-model_names = {'UMTS', 'LTE', '5G'}
-loss_model_names = {'plane earth loss', 'free space path loss', 'COST-231 hata'}
+model_names = {'900 Mhz', '1500 Mhz', '3.6 Ghz'};
+loss_model_names = {'plane earth loss', 'free space path loss', 'COST-231 hata'};
 ht = 40; % Transmitter antenna height in meters
 hr = 1.8;  % Receiver antenna height in meters
 epsilon_r = 17.2; % Relative permittivity of the ground
 c = 3e8;
+
+C_tunable = 45; % tunable paramter
 
 Cm = 15; %dB
 
@@ -20,10 +22,16 @@ for f = frequencies
     Ev_vals = [];
     Eh_vals = [];
 
+    Lray_vals = [];
+    Lf_vals = [];
+    L_vals = [];
+
     distances = []; % points from 100m to 10km
 
-    k = (2*pi)/(c/f);
+    k = (2*pi)/(lambda);
     for d = 100:1:10000
+        % -------------------------------- propagation models
+
         distances  = [distances, d];
 
         beta_angle = atan((ht + hr) / d);
@@ -43,29 +51,58 @@ for f = frequencies
         Ev_vals = [Ev_vals, 10*log(abs(Ev))];
         Eh_vals = [Eh_vals, 10*log(abs(Eh))];
         
+        % -------------------------------- loss models
+
+        % plane earth loss
+        Lray = 40*log10(d) - 20*log10(ht) - 20*log10(hr); % what is r ? 
+
+        % free space loss
+        Lf = 32.4 + 20*log10(d*10e-3) + 20*log10(f); % check if Rkm is correct
+
+        % okomura-hata
+        if (f >= 150e6) && (f <= 1500e6)
+          A = 69.65;
+          B = 26.16;
+        end
+
+        if (f >= 1500e6) && (f <= 3601e6) % adjusted for the sake of hata model
+          A = 46.3;
+          B = 33.9;
+        end
+        hr_small_city = (1.1*log10(f) - 0.7)*hr - (1.56*log10(f) - 0.8); % small city
+        L = A + B*log10(f) - 13.82*log10(ht) - hr_small_city + (C_tunable - 6.55*log10(ht))*log10(d*10-3) + Cm;
+
+        Lray_vals = [Lray_vals, -20*log10(Lray)];
+        Lf_vals = [Lf_vals, -20*log10(Lf)];
+        L_vals = [L_vals, -20*log10(L)];
     end
+    
+    break_point_d = (4*pi*hr*hr)/(lambda);
 
     figure(1);
     subplot(3, 1, counter);
     semilogx(distances, Eh_vals)
     hold on;
     semilogx(distances, Ev_vals);
+    hold off;
     title(model_names(counter));
     xlabel('Distance (m)');
-    ylabel('Received Power');
-    legend('show');
+    ylabel('Received Power [dB]');
+    xline(break_point_d,"r--")
+    legend('Eh','Ev','Break_point');
     grid on;
-
+  
     figure(2);
     subplot(3, 1, counter);
-    semilogx(distances, Eh_vals)
+    semilogx(distances, Lray_vals);
     hold on;
-    semilogx(distances, Ev_vals);
-    title(loss_model_names(counter));
-    xlabel('Distance (m)');
-    ylabel('Path loss');
-    legend('show');
-    grid on;
+    semilogx(distances, Lf_vals);
+    semilogx(distances, L_vals);
     hold off;
+    title(model_names(counter));
+    xlabel('Distance (m)');
+    ylabel('Path loss [-dB]');
+    legend('plane earth loss', 'free space loss','cost 231 loss');
+    grid on;
 end
 
