d4391b6cd221b54c7640f9aaa85abc8b8e6572e5
BL <borek.latinovic@powerdynax.com>
2023-12-07 00:10:56 +0200

    Some results
    TODO:
    results need to be reviewed and checked
    report needs to be made

--- project_solution.m
+++ project_solution.m
@@ -1,5 +1,9 @@
+clc;
+close all;
+clear all;
+
 % Constants
-frequencies = [900e6, 1800e6, 3600e6]; % UMTS, LTE, 5G frequencies
+frequencies = [900 * 1e6, 1800 * 1e6, 3600 * 1e6]; % UMTS, LTE, 5G frequencies
 model_names = {'900 Mhz', '1500 Mhz', '3.6 Ghz'};
 loss_model_names = {'plane earth loss', 'free space path loss', 'COST-231 hata'};
 ht = 40; % Transmitter antenna height in meters
@@ -18,6 +22,14 @@ counter = 0;
 for f = frequencies
     lambda = c / f; % Wavelength
     
+    distance_vector = 100:1:10000; % points from 100m to 10km
+    % total_dist = length(distance_vector) % total distance
+
+    % one of these is correct 
+    % beta_angle = asin(sqrt(epsilon_r - 1) / sqrt(epsilon_r^2 - 1)) % brewster angle
+
+    k = (2*pi)/(lambda);
+
     counter = counter + 1;
     Ev_vals = [];
     Eh_vals = [];
@@ -26,38 +38,54 @@ for f = frequencies
     Lf_vals = [];
     L_vals = [];
 
-    distances = []; % points from 100m to 10km
+    for o = 1:length(distance_vector)
 
-    k = (2*pi)/(lambda);
-    for d = 100:1:10000
+        d = distance_vector(o); % distance point
+
+        beta_angle = atan((ht + hr) / (d)); % 2 way reflection model - no epsilon_r ? 
+
         % -------------------------------- propagation models
 
-        distances  = [distances, d];
 
-        beta_angle = atan((ht + hr) / d);
         R1 = (ht/sin(beta_angle));
         R2 = (hr/sin(beta_angle));
         Rd = sqrt((ht - hr)^2 + d^2);
         Ri = R1 + R2;
         delta = Ri - Rd;
 
-        Rv = (-(epsilon_r*sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2)) / ((epsilon_r*sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2));
-        Rh = (sin(beta_angle)) - sqrt(epsilon_r - cos(beta_angle).^2) / ((sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2));
+        Rv = ((-epsilon_r * sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2)) / ...
+        ((epsilon_r*sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2));
+        
+        Rh = ((sin(beta_angle)) - sqrt(epsilon_r - cos(beta_angle).^2)) / ...
+        ((sin(beta_angle)) + sqrt(epsilon_r - cos(beta_angle).^2));
 
         E0 = (exp(-j*k*Rd)) / Rd;
+
+        % k_delta = ((4*pi)/(lambda)) * ((ht*hr)/d); % not sure if needed
+
         Ev = E0 * (1 + Rv*exp(-j*k*delta));
         Eh = E0 * (1 + Rh*exp(-j*k*delta));
+        
+        % Ev = ((Ev)^2)/377; % convert v/m to w/m^2 ??
+        % Eh = ((Eh)^2)/377;
 
-        Ev_vals = [Ev_vals, 10*log(abs(Ev))];
-        Eh_vals = [Eh_vals, 10*log(abs(Eh))];
+
+        Ev_vals = [Ev_vals, 20*log10(abs(Ev))]; % in db
+        Eh_vals = [Eh_vals, 20*log10(abs(Eh))];
         
         % -------------------------------- loss models
 
-        % plane earth loss
-        Lray = 40*log10(d) - 20*log10(ht) - 20*log10(hr); % what is r ? 
+        % plane earth loss - 2 ray model
+        % Lray = 40*log10(d) - 20*log10(ht) - 20*log10(hr); % what is r ? 
+        Lray = ((((4*pi*d))/lambda)^2) / ...
+          (4 * sin((2*pi*ht*hr)/(lambda*d)).^2);
+
+        if (lambda*d) > 2*pi*ht*hr
+          Lray = (d^4)/((ht*hr)^2);
+        end
 
         % free space loss
-        Lf = 32.4 + 20*log10(d*10e-3) + 20*log10(f); % check if Rkm is correct
+        Lf = 32.4 + 20*log10(d/1000) + 20*log10(f/1000000); % convert dist to km and frequency to Mhz
 
         % okomura-hata
         if (f >= 150e6) && (f <= 1500e6)
@@ -69,35 +97,35 @@ for f = frequencies
           A = 46.3;
           B = 33.9;
         end
-        hr_small_city = (1.1*log10(f) - 0.7)*hr - (1.56*log10(f) - 0.8); % small city
-        L = A + B*log10(f) - 13.82*log10(ht) - hr_small_city + (C_tunable - 6.55*log10(ht))*log10(d*10-3) + Cm;
+        hr_small_city = (1.1*log10(f) - 0.7)*hr - (1.56*log10(f) - 0.8); % small city, f in Mhz ??
+        L = A + B*log10(f/1000000) - 13.82*log10(ht) - hr_small_city + (C_tunable - 6.55*log10(ht))*log10(d/1000) + Cm;
 
-        Lray_vals = [Lray_vals, -20*log10(Lray)];
-        Lf_vals = [Lf_vals, -20*log10(Lf)];
-        L_vals = [L_vals, -20*log10(L)];
+        Lray_vals = [Lray_vals, -10*log10(Lray)];
+        Lf_vals = [Lf_vals, -Lf]; % already in dB
+        L_vals = [L_vals, -L]; % already in dB
     end
     
     break_point_d = (4*pi*hr*hr)/(lambda);
 
     figure(1);
     subplot(3, 1, counter);
-    semilogx(distances, Eh_vals)
+    semilogx(distance_vector, Eh_vals)
     hold on;
-    semilogx(distances, Ev_vals);
+    semilogx(distance_vector, Ev_vals);
     hold off;
     title(model_names(counter));
     xlabel('Distance (m)');
-    ylabel('Received Power [dB]');
+    ylabel('Received Power [dBm]');
     xline(break_point_d,"r--")
     legend('Eh','Ev','Break_point');
     grid on;
   
     figure(2);
     subplot(3, 1, counter);
-    semilogx(distances, Lray_vals);
+    semilogx(distance_vector, Lray_vals);
     hold on;
-    semilogx(distances, Lf_vals);
-    semilogx(distances, L_vals);
+    semilogx(distance_vector, Lf_vals);
+    semilogx(distance_vector, L_vals);
     hold off;
     title(model_names(counter));
     xlabel('Distance (m)');
